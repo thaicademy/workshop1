9a57857daf2c118dde086885ca7bfd673e00d006
siamcodes <somkiat@gmail.com>
2022-11-04 19:51:01 +0700

    UPDATE

--- checkout.php
+++ checkout.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
   session_start();
   include "connect.php";
   include "navbar.php";
--- save_checkout.php
+++ save_checkout.php
@@ -1,7 +1,7 @@
 <?php
+ob_start();
 session_start(); 
 include "connect.php";
-
 
  $Total = 0;
  $SumTotal = 0;
@@ -9,22 +9,25 @@ include "connect.php";
          VALUES(now(), '$_POST[name]', '$_POST[address]', '$_POST[tel]', '$_POST[email]' )";
  $result = mysqli_query($conn,$sql);
  
-  if(!$result){
+/*   if(!$result){
     echo $conn-> error;
     exit();
-  }
+  } */
 
   $strOrderID= mysqli_insert_id($conn); //กำหนดค่า order id
 
- for($i=0; $i <= $_SESSION["intLine"] ; $i++){
+ for($i=0; $i <= (int)$_SESSION["intLine"] ; $i++){
     if($_SESSION["strProductID"][$i] != ""){
         $sql= "INSERT INTO tbl_orderdetail(order_id, product_id, qty)
                VALUES('$strOrderID' ,'".$_SESSION["strProductID"][$i]."', '".$_SESSION["strQty"][$i]."')";
         $result = mysqli_query($conn, $sql);
+        if($result ){
+          mysqli_close($conn);
+          session_detroy(); 
+          header("Location: view_order.php");
+        }
     }
-    mysqli_close($conn);
-    session_detroy(); 
-    header("Location: view_order.php");
+    
  }
  
 
