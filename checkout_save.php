2c64f6df1ccb110b944177c327a8f7d3ad9fc0f0
siamcodes <somkiat@gmail.com>
2022-11-07 18:13:56 +0700

    update

--- checkout_save.php
+++ checkout_save.php
@@ -16,7 +16,7 @@ include "connect.php";
 
   $strOrderID= mysqli_insert_id($conn); //กำหนดค่า order id
 
- for($i=0; $i <= (int)$_SESSION["intLine"] ; $i++){
+ for($i=0; $i <= (int)$_SESSION["intLine"][$i] ; $i++){
     if($_SESSION["strProductID"][$i] != ""){
         $sql= "INSERT INTO tbl_orderdetail(order_id, product_id, qty)
                VALUES('$strOrderID' ,'".$_SESSION["strProductID"][$i]."', '".$_SESSION["strQty"][$i]."')";
