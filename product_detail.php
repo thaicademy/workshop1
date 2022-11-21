e50f301706c6376e13d434949c672fb3249112d3
siamcodes <somkiat@gmail.com>
2022-11-21 18:52:28 +0700

    update

---
+++ product_detail.php
@@ -0,0 +1,29 @@
+<?php
+ session_start();
+ $product_id = $_GET['product_id'];
+ include "navbar.php";
+
+ $sql = "SELECT * FROM tbl_products WHERE product_id='$product_id' ";
+ $result = mysqli_query($conn, $sql);
+ $row = mysqli_fetch_assoc($result);
+?>
+
+<div class="container pt-2">
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="<?= $row['img'] ?>" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $row['name']; ?></h5>
+                    <p class="card-text">ราคา <?= $row['price']; ?> บาท</p>
+                    <p class="card-text">คงเหลือ <?= $row['stock']; ?></p>
+                    <p class="card-text">ประเภทสินค้า <?= $row['cat_name']; ?></p>
+                    <p class="card-text"> <?= $row['description']; ?></p>
+                    <a href="order.php?product_id=<?php echo $row[product_id]; ?>" class="btn btn-success" >สั่งซื้อสินค้า</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
